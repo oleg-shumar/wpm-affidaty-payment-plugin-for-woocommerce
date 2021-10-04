f176e1cdc5f1abb5e57fa09e8c46944b17be4a38
Oleg Shumar <oleg.shumar@gmail.com>
2021-10-04 16:01:49 +0300

    Webhooks and URLs added

--- affidaty-woo.php
+++ affidaty-woo.php
@@ -66,10 +66,32 @@ add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wc_affidaty_g
 add_action( 'plugins_loaded', 'wc_affidaty_gateway_init', 9999 );
 
 /**
+ * Virtual page where Affidaty payment page will be displayed
+ */
+add_filter( 'init', function ( $template ) {
+	if ( isset( $_GET['affidaty_id'] ) && isset( $_REQUEST['_wpnonce'] ) && wp_verify_nonce( $_REQUEST['_wpnonce'], 'display_payment_page' ) && get_post_type( (int) $_GET['affidaty_id'] ) == "shop_order" ) {
+		$order_id  = (int) $_GET['affidaty_id'];
+		$affidiaty = new WC_Gateway_Affidaty();
+		$affidiaty->display_payment_page( $order_id );
+		wp_die();
+	}
+} );
+
+/**
  * Init Payment Gateway
  */
 function wc_affidaty_gateway_init() {
 	class WC_Gateway_Affidaty extends WC_Payment_Gateway {
+
+		/**
+		 * API Root Endpoint
+		 */
+		const AFFIDATY_API_ROOT_ENDPOINT = 'https://dev.exchange.affidaty.net';
+
+		/**
+		 * API Pay Endpoint
+		 */
+		const AFFIDATY_API_PAY_ENDPOINT = 'https://dev.exchange.affidaty.net/api/fastPay/pay/';
 
 		/**
 		 * Constructor for the gateway.
@@ -95,7 +117,7 @@ function wc_affidaty_gateway_init() {
 			add_action( 'woocommerce_thankyou_' . $this->id, array( $this, 'thankyou_page' ) );
 
 			// API Logic
-			add_action( 'woocommerce_api_' . strtolower( "WC_Gateway_Affidaty" ), array( $this, 'check_ipn_response' ) );
+			add_action( 'woocommerce_api_' . 'wc_affidaty', array( $this, 'check_ipn_response' ) );
 
 			// Customer Emails
 			add_action( 'woocommerce_email_before_order_table', array( $this, 'email_instructions' ), 10, 3 );
@@ -166,12 +188,41 @@ function wc_affidaty_gateway_init() {
 		 * Webhook logic will be here
 		 */
 		public function web_hook_handler() {
+			$order_id = (int) $_GET['order_id'];
+
+			if ( function_exists( 'phpversion' ) && version_compare( phpversion(), '5.6', '>=' ) ) {
+				$post_data = file_get_contents( 'php://input' );
+				$post_data = json_decode( $post_data, true );
+
+				if ( isset( $post_data['status'] ) ) {
+					if ( $order_id > 0 ) {
+						$affidaty_webhook_triggered = (int) get_post_meta( $order_id, '_affidaty_webhook_triggered', true );
+						if ( $affidaty_webhook_triggered == 1 ) {
+							exit;
+						}
+					}
+
+					$order      = new WC_Order( $order_id );
+					$order_data = $order->get_data();
+
+					$order->add_meta_data( '_affidaty_webhook_triggered', 1 );
+					$order->add_meta_data( 'affidaty_transaction_id', sanitize_title( $post_data['_id'] ) );
+					$order->save_meta_data();
+
+					if ( $post_data['status'] == 'COMPLETE' ) {
+						$order->update_status( 'processing', __( 'Payment successful. Transaction Id: ' . sanitize_title( $post_data['_id'] ), 'affidaty-woo' ) );
+					}
+				}
+			}
+
+			exit;
 		}
 
 		/**
 		 * IPN Check will be here
 		 */
 		public function check_ipn_response() {
+			$this->web_hook_handler();
 		}
 
 		/**
@@ -193,9 +244,110 @@ function wc_affidaty_gateway_init() {
 		}
 
 		/**
+		 * Make form and send it to API
+		 *
+		 * @param $order_id
+		 */
+		public function display_payment_page( $order_id ) {
+			$order_id = (int) $order_id;
+
+			// Grab the order
+			$order = wc_get_order( $order_id );
+
+			$order_totals = array();
+
+			foreach ( $order->get_items() as $item_id => $item ) {
+				$order_totals[] = array(
+					'name'   => $item->get_name(),
+					'amount' => $item->get_subtotal(),
+				);
+			}
+
+			$order_totals[] = array(
+				'name'   => 'Shipping',
+				'amount' => $order->get_shipping_total(),
+			);
+
+			$order_totals[] = array(
+				'name'   => 'Tax',
+				'amount' => $order->get_total_tax(),
+			);
+
+			$params = array(
+				'applicationid'   => $this->get_option( 'application_id' ),
+				'email'           => $order->get_billing_email(),
+				'amount'          => $order->get_total(),
+				'currency'        => $order->get_currency(),
+				'webhook'         => add_query_arg( array(
+					'wc-api'   => 'wc_affidaty',
+					'order_id' => $order_id,
+				), get_site_url() ),
+				'redirectSuccess' => $order->get_checkout_order_received_url(),
+				'redirectCancel'  => add_query_arg( array(
+					'cancelled' => '1',
+				), wc_get_checkout_url() ),
+				'hmac'            => $this->get_option( 'secret_token' ),
+				'p'               => $order_totals,
+			);
+
+			?>
+            <p><?php echo __( 'Loading...', 'affidaty-woo' ) ?></p>
+            <form id="affidaty_payment_form" action="<?= esc_attr( $this::AFFIDATY_API_PAY_ENDPOINT ) ?>" method="POST">
+                <input type="hidden" id="aff_applicationid" name="applicationid" value="<?= esc_attr( $params['applicationid']) ?>" />
+                <input type="hidden" id="aff_email" name="email" value="<?= esc_attr( $params['email'] ) ?>">
+                <input type="hidden" id="aff_amount" name="amount" value="<?= esc_attr( $params['amount'] ) ?>">
+                <input type="hidden" id="aff_currency" name="currency" value="<?= esc_attr( $params['currency'] ) ?>">
+                <input type="hidden" id="aff_webhook" name="webhook" value="<?= esc_url( $params['webhook'] ) ?>">
+                <input type="hidden" id="aff_redirectSuccess" name="redirectSuccess" value="<?= esc_attr( $params['redirectSuccess'] ) ?>">
+                <input type="hidden" id="aff_redirectCancel" name="redirectCancel" value="<?= esc_attr( $params['redirectCancel'] ) ?>">
+                <input type="hidden" id="aff_hmac" name="hmac" value="<?= esc_attr( $params['hmac'] ) ?>">
+				<?php foreach($order_totals as $key => $order_total){ ?>
+                    <input type="hidden" name="p[<?= (int)$key ?>][name]" value="<?= esc_attr( $order_total['name'] )?>" />
+                    <input type="hidden" name="p[<?= (int)$key ?>][amount]" value="<?= esc_attr( $order_total['amount'] )?>" />
+				<?php } ?>
+            </form>
+            <script>
+                window.onload = function () {
+                    document.getElementById('affidaty_payment_form').submit();
+                    return false;
+                };
+            </script>
+			<?php
+			exit;
+		}
+
+		/**
+		 * Display description and alert if needed
+		 */
+		function payment_fields() {
+			?>
+            <div class="form-row form-row-wide">
+                <p><?php echo $this->description; ?></p>
+				<?php
+				if ( isset( $_REQUEST['cancelled'] ) ) {
+					?>
+                    <script>
+                        let message = '<div class="woocommerce-NoticeGroup woocommerce-NoticeGroup-checkout"><div class="woocommerce-error"><?php echo __( 'Payment canceled by customer', 'affidaty-woo' )?></div></div>';
+                        jQuery(document).ready(function () {
+                            jQuery('.woocommerce-notices-wrapper:first').html(message);
+                        });
+                    </script>
+					<?php
+				}
+				?>
+            </div>
+			<?php
+		}
+
+		/**
 		 * Process the payment and return the result
 		 */
 		public function process_payment( $order_id ) {
+			return array(
+				'result'   => 'success',
+				'redirect' => html_entity_decode( wp_nonce_url( site_url( '/affidaty/?affidaty_id=' . $order_id ), 'display_payment_page' ) ),
+			);
+
 			$order = wc_get_order( $order_id );
 
 			// Mark as on-hold (we're awaiting the payment)
